51fae77d775dd0b56c2912c5ef3767242098d37b
mraihanaf <mraihanaf0@gmail.com>
2025-03-31 00:00:58 +0700

    feat: enable gnome extensions

--- desktop/gnome.dconf.nix
+++ desktop/gnome.dconf.nix
@@ -4,10 +4,10 @@
 {
   programs.dconf.profiles = {
 
-    user.databases = [{
+    rai.databases = [{
       settings = {
           "org/gnome/shell" = {
-      disable-user-extensions = true;
+      disable-user-extensions = false;
       disabled-extensions = [ "native-window-placement@gnome-shell-extensions.gcampax.github.com" "windowgestures@extension.amarullz.com" "status-icons@gnome-shell-extensions.gcampax.github.com" "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com" "system-monitor@gnome-shell-extensions.gcampax.github.com" "user-theme@gnome-shell-extensions.gcampax.github.com" "window-list@gnome-shell-extensions.gcampax.github.com" "windowsNavigator@gnome-shell-extensions.gcampax.github.com" ];
       enabled-extensions = [ "blur-my-shell@aunetx" "apps-menu@gnome-shell-extensions.gcampax.github.com" "drive-menu@gnome-shell-extensions.gcampax.github.com" "workspace-indicator@gnome-shell-extensions.gcampax.github.com" "places-menu@gnome-shell-extensions.gcampax.github.com" "auto-move-windows@gnome-shell-extensions.gcampax.github.com" "rectangle@acristoffers.me" ];
       last-selected-power-profile = "power-saver";
